7052347fc4664729b198a28513104cc7b466fdb6
Princemaddheshiya <92073680+Princemaddheshiya@users.noreply.github.com>
2021-10-15 19:33:00 +0530

    Find leap year

---
+++ leap year .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+int a;
+printf("enter the year :");
+scanf("%d",&a) ;
+if(a%4)
+
+
+printf("this year is not leap year ");
+else
+printf("this year is leap year ");
+
+}
