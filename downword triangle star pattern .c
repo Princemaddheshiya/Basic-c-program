36d3e2b7c91a9eb2e68501c593223c581fb7dc00
Princemaddheshiya <92073680+Princemaddheshiya@users.noreply.github.com>
2021-11-03 15:53:48 +0530

    Add files via upload
    downword triangle star pattern .c

---
+++ downword triangle star pattern .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main() 
+{
+int row,i,j;
+printf("enter the number of row::");
+scanf("%d",&row);
+for(i=1; i<=row; i++)
+
+{
+
+for(j=1; j<=row-i; j++)
+
+printf("*");
+printf("\n");
+}
+}
